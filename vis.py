2e55ddf758e603acf1fd8c0862032e637d68095b
Katie Dillon <kedillon@umich.edu>
2019-04-13 17:51:59 -0400

    implemented word cloud for reddit

--- vis.py
+++ vis.py
@@ -2,6 +2,7 @@ from reddit import scrape_reddit_politics
 import sqlite3
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+import spacy
 
 def generate_word_cloud(reddit=True):
 
@@ -12,17 +13,25 @@ def generate_word_cloud(reddit=True):
     cur.execute('SELECT title FROM Reddit')
     title_list = cur.fetchall()
 
-    all_words = []
+    #natural language processing
+    nlp = spacy.load("en_core_web_sm")
+
+    #all_words = []
     words_dict = {}
+    #words to ignore in most common
+    ignore = ['to', 'of', 'in', 'is', 'on', 'and', 'for', 'it', 'as', 'a', 'not', "n't", "that", "the", "or", "with", "are", "we", "’s", "'s", "his", "say", "says", "from", "do", "be", "he", "i", "out", "at", "after", "new", "about", "by", "was", "has"]
+
     for tup in title_list:
-        words = tup[0].split()
-        for word in words:
+        #words = tup[0].split()
+        doc = nlp(tup[0])
+        for token in doc:
+            word = token.text
             word = word.rstrip('.')
             word = word.lower()
 
-            words_dict[word] = words_dict.get(word,0) + 1
-            all_words.append(word)
-
+            if not word in ignore and not token.is_punct:
+                words_dict[word] = words_dict.get(word,0) + 1
+                #all_words.append(word)
 
     wc = WordCloud(background_color="white",width=1000,height=1000, max_words=30,relative_scaling=0.5,normalize_plurals=False).generate_from_frequencies(words_dict)
     plt.axis("off")
