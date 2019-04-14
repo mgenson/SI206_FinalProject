4839f4cb9410907b9cdd930cd58f1063c98f2b39
Eva Smith <Eva@0587396608.wireless.umich.net>
2019-04-14 13:02:19 -0400

    Code connects to NewAPI

--- news.py
+++ news.py
@@ -2,4 +2,19 @@
 # Final Project
 # Eva Smith
 
-import news_info.py
+from news_info import news_api
+from newsapi import NewsApiClient
+import sqlite3
+
+
+def news_scrape():
+    api = NewsApiClient(api_key = news_api)
+
+    conn = sqlite3.connect('Eva_test.sqlite')
+    cur = conn.cursor()
+
+    #cur.execute('CREATE TABLE IF NOT EXISTS News()')
+    requests = api.get_everything(q='politics') #returns dictionary
+
+if __name__ == "__main__":
+    news_scrape()
