7c5b99e43967a0792207a454add6c2e80b995b7a
mgenson <46972714+mgenson@users.noreply.github.com>
2019-04-18 14:40:20 -0400

    Got git working

--- NYTimes.py
+++ NYTimes.py
@@ -10,32 +10,34 @@ def scrape_nyt_politics():
     obama_dict = api.search(q = 'Immigration', begin_date = 20080120, end_date = 20080429)
     trump_dict = api.search(q = "Immigration", begin_date = 20160120, end_date = 20160429)
     #getting into docs
-    bush = bush_dict["response"]
-    obama = obama_dict["response"]
-    trump = trump_dict["response"]
-    print(bush)
+    bush = bush_dict["response"]["docs"][0]
+    
+    obama = obama_dict["response"]['docs'][0]
+    trump = trump_dict["response"]["docs"][0]
+    print("BUSH", bush,"OBAMA", obama,"TRUMP", trump)
     # Make bush json
     dumped_json_bush = json.dumps(bush)
-    bw = open("bush_json","w")
-    bw.write(dumped_json_bush)
-    bw.close() 
+    bush_json = open(bush ,"w")
+    bush_json.write(dumped_json_bush)
+    bush_json.close() 
     #make obama json
     dumped_json_obama = json.dumps(obama)
-    ow = open("obama_json", "w")
-    ow.write(dumped_json_obama)
-    ow.close
+    obama_json = open(obama, "w")
+    obama_json.write(dumped_json_obama)
+    obama_json.close
     #make trump json
     dumped_json_trump = json.dumps(trump)
-    tw = open
-    tw.write("trump_json", "w")
-    tw.close()
-def data_nyt(dumped_json_bush, dumped_json_obama, dumped_json_trump):   
+    trump_json = open(trump, "w")
+    trump_json.write(dumped_json_trump)
+    trump_json.close()
+#def data_nyt(dumped_json_bush, dumped_json_obama, dumped_json_trump):   
 
     #make connection to database
-    conn = sqlite3.connect("NYT.sqlite")
-    cur = conn.cursor()
-    #cur.execute("CREATE TABLE IF DOESN'T EXIST")
-def visual_nyt()
+    #conn = sqlite3.connect("NYT.sqlite")
+    #cur = conn.cursor()
+    #cur.execute("CREATE TABLE IF DOESN'T EXIST NYT")
+    #cur.execute("CREATE TABLE NYT(")
+#def visual_nyt()
     
     
 if __name__ == '__main__':
